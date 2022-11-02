f455f50bdfc9b64a17995be5a45fa9829809f2ee
Jason Keung <jk4818@ic.ac.uk>
2022-11-02 21:17:54 +0000

    Feat: Added search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import SearchBar from './components/SearchBar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SearchBar from '../components/SearchBar';
+
+function Hero() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Hero;
