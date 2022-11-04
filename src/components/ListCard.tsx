a4c39a6b3e82a6fa6fa66d2c1ec3071da8d2c6eb
Jason Keung <work.jk4818@gmail.com>
2022-11-04 19:12:31 +0000

    Feat: added opacity and colour correction on cards

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -4,7 +4,7 @@ function ListCard() {
   return (
     <div className="w-72 h-80">
       <div
-        className="w-full p-4 aspect-square flex flex-col gap-4 text-center font-raleway bg-white rounded-2xl drop-shadow-main
+        className="w-full p-4 aspect-square flex flex-col gap-4 text-center font-raleway bg-white rounded-2xl drop-shadow-main bg-opacity-70
       0"
       >
         <div className="mx-auto -mt-14 w-40 aspect-square">
@@ -18,10 +18,10 @@ function ListCard() {
         <h2 className="text-xs text-web-gray">Highball Glass</h2>
 
         <ul className="mt-auto flex gap-4">
-          <li className="w-max h-max text-xs font-extrabold bg-red-400 rounded-lg px-4 p-2">
+          <li className="w-max h-max text-xs font-extrabold bg-white rounded-lg px-4 p-2">
             Rum
           </li>
-          <li className="w-max h-max text-xs font-extrabold bg-red-400 rounded-lg px-4 p-2">
+          <li className="w-max h-max text-xs font-extrabold bg-white rounded-lg px-4 p-2">
             Rum
           </li>
         </ul>
