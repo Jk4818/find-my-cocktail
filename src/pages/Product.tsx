8736e530536d1d6f52509d978d8fc675a0cfe505
Jason Keung <work.jk4818@gmail.com>
2022-11-07 16:59:52 +0000

    Fix: Corrected Carousel Chevron

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -69,7 +69,7 @@ function Product() {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    nextArrow: <GoChevronLeft />,
+    nextArrow: <GoChevronRight />,
     prevArrow: <GoChevronLeft />,
   };
 
