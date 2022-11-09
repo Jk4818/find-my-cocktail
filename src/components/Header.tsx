759a97ff974569c5d184d33eedde889b759d0c2d
Jason Keung <work.jk4818@gmail.com>
2022-11-09 16:30:40 +0000

    Feat: Added responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div className='relative w-full h-36 flex justify-center  items-center'>
-      <div className='absolute w-full'>
+    <div className='relative w-full h-36 flex px-4 md:px-20 justify-end md:justify-center  items-center'>
+      <div className='absolute px-8 md:px-20 w-full'>
         <Logo />
       </div>
       <SearchBar />
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,13 +6,13 @@ type Props = {};
 
 export default function Logo({}: Props) {
   return (
-    <div className="w-60 h-12 flex items-center gap-2">
+    <div className="w-60 h-10 flex items-center gap-2">
       <img
         src={LogoImg}
         alt="new"
         className="h-full aspect-square mix-blend-multiply rounded-xl object-cover animate-ring"
       />
-      <h1 className="font-raleway font-black">Find My Cocktail</h1>
+      <h1 className="hidden lg:block font-raleway font-black text-sm">Find My Cocktail</h1>
     </div>
   );
 }
