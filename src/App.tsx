920e3e83245fce6fffd879dd81ce4bdee0fa5fbe
Jason Keung <work.jk4818@gmail.com>
2022-11-10 17:07:14 +0000

    Feat: Created layout component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router,Routes,  Route } from "react-router-dom";
 
 import BlobAnimation from "./components/BlobAnimation";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Hero from "./pages/Hero";
 import Product from "./pages/Product";
 import Search from "./pages/Search";
@@ -12,10 +10,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <header className="fixed w-screen h-screen">
+        <div className="fixed w-screen h-screen">
           <BlobAnimation />
-        </header>
-        <Header />
+        </div>
 
         <Routes>
           <Route path="/" element={<Hero />} />
@@ -23,7 +20,6 @@ function App() {
           <Route path="/product/:cocktailId" element={<Product />} />
         </Routes>
 
-        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <div>
+      <Header />
+
+      {children}
+      <Footer />
+    </div>
+  );
+}
