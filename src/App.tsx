b3c627e98a3af61146f4fd4acbcf781ef193702c
Jason Keung <work.jk4818@gmail.com>
2022-11-12 11:23:28 +0000

    Feat: Added initial 404 component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,  Route } from "react-router-dom";
 
 import BlobAnimation from "./components/BlobAnimation";
 import Layout from "./components/Layout";
+import NotFound from "./pages/404";
 import Hero from "./pages/Hero";
 import Product from "./pages/Product";
 import Search from "./pages/Search";
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Hero />} />
             <Route path="/search" element={<Search />} />
             <Route path="/product/:cocktailId" element={<Product />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import NoResults from '../components/NoResults'
 
 type Props = {}
 
-export default function NotFound({}: Props) {
-  return (
-    <div>404</div>
-  )
+export default function NotFound({ }: Props) {
+    return (
+        <div>
+            404
+            <NoResults />
+        </div>
+    )
 }
