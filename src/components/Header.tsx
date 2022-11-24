ff63028432639a21c175487315eeaf2140b3aaf6
Jason Keung <work.jk4818@gmail.com>
2022-11-24 18:27:48 +0000

    Feat: created useffect for header search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Logo from './Logo'
 import SearchBar from './SearchBar'
+
+import { useLocation } from "react-router-dom";
 
 type Props = {}
 
 export default function Header({}: Props) {
+
+  const location = useLocation();
+  useEffect(() => {
+    if(location.pathname === "/") {
+
+    }
+      
+  }, [])
+  
+  
   return (
     <div className='relative w-full h-20 lg:h-36 flex px-4 md:px-20 justify-end md:justify-center  items-center'>
       <div className='absolute px-8 md:px-20 w-full'>
