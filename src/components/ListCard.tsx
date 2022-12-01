f5e59e690b3b40836a8e74591ee0b59dafab6d2a
Jason Keung <work.jk4818@gmail.com>
2022-12-01 12:15:47 +0000

    Fix: Testing optimise removing hover translation from listcard

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -42,7 +42,7 @@ function ListCard({ loading, cocktailData }: Props) {
 
   return (
     <div className="w-72 h-72 lg:w-72 lg:h-72" onClick={handleClick}>
-      <div className="w-full h-full p-4 aspect-square flex flex-col gap-4 text-center font-raleway bg-white/80 backdrop-blur-sm rounded-2xl drop-shadow-main  group hover:-translate-y-2 transition-all cursor-pointer">
+      <div className="w-full h-full p-4 aspect-square flex flex-col gap-4 text-center font-raleway bg-white/80 backdrop-blur-sm rounded-2xl drop-shadow-main  group transition-all cursor-pointer">
         <div className="mx-auto -mt-14 w-40 aspect-square">
           {!loading ? (
             <LazyLoad height={160}>
